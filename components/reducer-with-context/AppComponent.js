14d320a4a1799d80692a907f9f33bf7fa8520eeb
Sean <sean@vestaridefair.com>
2021-08-14 14:56:30 -0700

    all setup and ready for reducer with context

--- components/reducer-with-context/AppComponent.js
+++ components/reducer-with-context/AppComponent.js
@@ -1,22 +1,27 @@
-import { Heading, Text } from '@chakra-ui/react'
+import { Heading, Text } from '@chakra-ui/react';
 
-const { default: ComponentA } = require('./ComponentA')
-const { default: ComponentB } = require('./ComponentB')
-const { default: ComponentC } = require('./ComponentC')
+const { default: ComponentA } = require('./ComponentA');
+const { default: ComponentB } = require('./ComponentB');
+const { default: ComponentC } = require('./ComponentC');
 
+const AppComponent = () => (
+  <div>
+    <Heading fontSize="lg" fontWeight="semibold">
+      {' '}
+      Our goal is to maintain a count state within AppComponent.js and modify
+      that state from components A, D and F.{' '}
+    </Heading>{' '}
+    <br />
+    <Text>
+      To accomplish this we have two main steps. Step 1 is to create the counter
+      within AppComponent.js using the reducer hook. Step 2 is to provide and
+      consume the context within the required components.
+    </Text>
+    <br />
+    <ComponentA />
+    <ComponentB />
+    <ComponentC />
+  </div>
+);
 
-const AppComponent = () => {
-  return (
-    <div>
-    <Heading fontSize="lg" fontWeight="semibold"> Our goal is to maintain a count state within AppComponent.js and modify that state from components A, D and F. </Heading>
-    <Text>To accomplish this we have two main steps. Step 1 is to create the counter within AppComponent.js using the reducer hook. Step 2 is to provide and consume the context within the required components.</Text>
-      <ComponentA />
-      <ComponentB />
-      <ComponentC />
-
-    </div>
-  )
-}
-
-export default AppComponent
-
+export default AppComponent;
