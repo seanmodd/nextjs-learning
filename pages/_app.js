ac4d75c32c5c025b68b665b90df85d206a8ba5a8
Sean <sean@vestaridefair.com>
2021-08-17 13:50:10 -0700

    nice

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Provider session={pageProps.session}>
       <ChakraProvider theme={theme}>
+        <Navbar />
         <Component {...pageProps} />
       </ChakraProvider>
     </Provider>
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,23 +8,23 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  database: process.env.DB_URL,
-  session: {
-    jwt: true,
-  },
-  jwt: {
-    secret: 'asdcvbtjhm',
-  },
-  callbacks: {
-    async jwt(token, user) {
-      if (user) {
-        token.id = user.id;
-      }
-      return token;
-    },
-    async session(session, token) {
-      session.user.id = token.id;
-      return session;
-    },
-  },
+  // database: process.env.DB_URL,
+  // session: {
+  //   jwt: true,
+  // },
+  // jwt: {
+  //   secret: 'asdcvbtjhm',
+  // },
+  // callbacks: {
+  //   async jwt(token, user) {
+  //     if (user) {
+  //       token.id = user.id;
+  //     }
+  //     return token;
+  //   },
+  //   async session(session, token) {
+  //     session.user.id = token.id;
+  //     return session;
+  //   },
+  // },
 });
